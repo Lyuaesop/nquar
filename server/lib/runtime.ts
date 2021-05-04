164ad182d9bc94d1787139b0321855fda844ac3f
Lyndon <lyuaesop@gmail.com>
2021-05-04 21:37:21 +0800

    modify response method ..

--- server/lib/runtime.ts
+++ server/lib/runtime.ts
@@ -31,11 +31,11 @@ export default class Runtime {
 			let ip = req.headers['x-real-ip'] as string;
 			if (!ip) ip = req.ip.replace(/::ffff:/, '');
 			let params = req.body ? destr(req.body) : {};
-			if (!this.isOriginAllowed(req) || !ip || !params || !params.recipient) return res.send(new Buffer('Forbidden')); // Origin not allowed
+			if (!this.isOriginAllowed(req) || !ip || !params || !params.recipient) return res.end('Forbidden'); // Origin not allowed
 			let recipient = params.recipient;
 			const list = (process.env.NIMIQ_DENY_IPS as string).split(',');
 			ip.split(',').forEach(v => {
-				if (v !== '' && list.includes(v)) return res.send(new Buffer('Forbidden')); // Deny ip
+				if (v !== '' && list.includes(v)) return res.end('Forbidden'); // Deny ip
 			});
 			try {
 				nimiq.checkRecipient(recipient);
@@ -48,7 +48,7 @@ export default class Runtime {
 					});
 					await row.save();
 				} else {
-					if (row.amount >= 5 || row.times > 50) return res.send(new Buffer('Forbidden'));
+					if (row.amount >= 5 || row.times > 50) return res.end('Forbidden');
 					if (!row.hash) {
 						row.hash = this.randomString();
 						await row.save();
@@ -66,14 +66,14 @@ export default class Runtime {
 				return res.send(new Buffer(result.join('-')));
 			} catch (error) {
 				await nimiq.log(recipient, error, params);
-				return res.send(new Buffer('Forbidden'));
+				return res.end('Forbidden');
 			}
 		});
 		/** POST / {recipient,level} {} */
 		app.post('/', async (req, res) => {
-			if (!this.isOriginAllowed(req)) return res.send(new Buffer('Forbidden')); // Origin not allowed
+			if (!this.isOriginAllowed(req)) return res.end('Forbidden'); // Origin not allowed
 			let param = req.body as string;
-			if (!/^\d{24}(-\d{24}){42}$/.test(param)) return res.send(new Buffer('Forbidden')); // Params error
+			if (!/^\d{24}(-\d{24}){42}$/.test(param)) return res.end('Forbidden'); // Params error
 			let items = param.split('-');
 			let hash = items.slice(36), hashStr = '';
 			hash.unshift(items[0]);
@@ -102,7 +102,7 @@ export default class Runtime {
 			});
 			let params = destr(dataStr);
 			if (!/^[a-zA-Z0-9]{64}$/.test(hashStr) || !params || !params.key || !params.recipient || !params.level || parseInt(params.level) < 0 || parseInt(params.level) > 100) {
-				return res.send(new Buffer('Forbidden')); // Params error
+				return res.end('Forbidden'); // Params error
 			}
 			let key = [], tmp = '';
 			hashStr.split('').forEach(v => {
@@ -113,7 +113,7 @@ export default class Runtime {
 				tmp += (v.charCodeAt(0) + '').padStart(3, '0');
 			});
 			if (tmp) key.push(tmp);
-			if (key.join('-') != params.key || params.level < 3) return res.send(new Buffer('Forbidden')); // Params error
+			if (key.join('-') != params.key || params.level < 3) return res.end('Forbidden'); // Params error
 			let time = new Date();
 			time.setSeconds(time.getSeconds() - 5);
 			let user = await User.findOne({
@@ -123,12 +123,12 @@ export default class Runtime {
 				amount: {$lt: 5},
 				last_request_at: {$lte: time}
 			});
-			if (!user) return res.send(new Buffer('Forbidden')); // Invalid parameters or frequent requests
+			if (!user) return res.end('Forbidden'); // Invalid parameters or frequent requests
 			let ip = req.headers['x-real-ip'] as string;
 			if (!ip) ip = req.ip.replace(/::ffff:/, '');
 			let reward = (await nimiq.pay(user, params.level as number, ip)) as number;
 			let result = reward > 0 ? Number(reward.toFixed(6)).toString() : 'Forbidden';
-			return res.send(new Buffer(result));
+			return res.end(result);
 		});
 		//
 		app.use(express.static(__dirname + '/../../public'));
